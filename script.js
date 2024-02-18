db81356bd1211b17b918ad081e68264550cf7ec6
David Nguyen <51886638+daviddhnguyen@users.noreply.github.com>
2024-02-18 11:59:12 -0800

    added basic html elements

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 const myLibrary = [];
 
-function Book() {
+function Book(title, author, read) {
   // the constructor...
+  this.title = title;
+  this.author = author;
+  this.read = read;
 }
 
 function addBookToLibrary() {
   // do stuff here
+  
+}
+
+Book.prototype = {
+  read      : function () {return 'read'; },
+  notRead  : function () {return 'not read yet'; }
 }
