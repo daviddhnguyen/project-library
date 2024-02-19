4da2b991f2b4a591bc804c139da4b6150a13e2c8
David Nguyen <51886638+daviddhnguyen@users.noreply.github.com>
2024-02-19 11:51:22 -0800

    add dialog box and icon

--- script.js
+++ script.js
@@ -7,6 +7,27 @@ const book3 = new Book('The Count of Monte Cristo', 'Alexandre Dumas', 1276, 'Re
 
 myLibrary.push(book1, book2, book3);
 
+//EVENT LISTENERS
+const addBook = document.querySelector('#addBook-btn');
+const addBookDialog = document.querySelector('#addBook');
+const newTitle = addBookDialog.querySelector('#title');
+const newAuthor = addBookDialog.querySelector('#author');
+const newPages = addBookDialog.querySelector('#pages');
+const newStatus = addBookDialog.querySelector('#status');
+const cancelBook = addBookDialog.querySelector('.cancel-addBook')
+const submitBook = addBookDialog.querySelector('.submit-addBook') 
+
+//DIALOG CODE
+addBook.addEventListener('click', () => {
+  addBookDialog.showModal();
+});
+
+//CANCEL BUTTON
+cancelBook.addEventListener('click', (e) => {
+  addBookDialog.close();
+})
+
+
 function Book(title, author, pages, status) {
   // the constructor...
   this.title = title;
@@ -25,13 +46,12 @@ function addBookToLibrary(title, author, pages, status) {
 
 function renderCards() {
   myLibrary.forEach((book, index) => {
-
   })
 }
 
-function addBookEventListener() {
-  const addBook = document.querySelector('.addBook-button');
-  addBook.addEventListener('click', ()=> {
-    const form = document.
-  })
-}
+// function addBookEventListener() {
+//   const addBook = document.querySelector('.addBook-button');
+//   addBook.addEventListener('click', ()=> {
+//     const form = document.
+//   })
+// }
