e8f3d822a298d0ec060d7e08670c2fdc448738f8
David Nguyen <51886638+daviddhnguyen@users.noreply.github.com>
2024-02-18 20:03:07 -0800

    added css styling for cards

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
 const myLibrary = [];
 
-function Book(title, author, read) {
+//EXAMPLE BOOKS
+const book1 = new Book('Ulysses', 'James Joyce', 732, 'Not read');
+const book2 = new Book('Don Quixote', 'Miguel de Cervantes', 1072, 'Not read');
+const book3 = new Book('The Count of Monte Cristo', 'Alexandre Dumas', 1276, 'Read');
+
+myLibrary.push(book1, book2, book3);
+
+function Book(title, author, pages, status) {
   // the constructor...
   this.title = title;
   this.author = author;
-  this.read = read;
+  this.pages = pages;
+  this.status  = status;
 }
 
-function addBookToLibrary() {
+function addBookToLibrary(title, author, pages, status) {
   // do stuff here
+  const newBook = new Book(title, author, pages, status);
+  myLibrary.push(newBook);
   
+  renderCards()
 }
 
-Book.prototype = {
-  read      : function () {return 'read'; },
-  notRead  : function () {return 'not read yet'; }
+function renderCards() {
+  myLibrary.forEach()
 }
